77932c961c8560af8c0c1d4778deeef5044d2c0c
John Kosta <kostajohn@ymail.com>
2020-10-05 08:46:04 -0700

    d25

--- d25.java
+++ d25.java
@@ -1,4 +1,4 @@
-//array2 sum67 
+//array2 sum67
 
 public int sum67(int[] nums) {
   boolean flag = true;
@@ -20,4 +20,29 @@ public int sum67(int[] nums) {
 
 
   } return sumOfArray;
+}
+
+
+//array2 lucky13
+public boolean lucky13(int[] nums) {
+  int countOneThree = 0;
+
+  for (int i = 0; i < nums.length; i++) {
+    if (nums[i] == 1 || nums[i] == 3) {
+      countOneThree++;
+    }
+  } return countOneThree == 0;
+}
+
+//array2 Has22
+public boolean has22(int[] nums) {
+  boolean twoTwo = false;
+
+  for (int i = 0; i < nums.length-1; i++){
+    if (nums[i] == 2 && nums[i+1] == 2){
+      twoTwo = true;
+    }
+  }
+
+  return twoTwo;
 }
