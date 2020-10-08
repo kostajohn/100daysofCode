1045749048f76026e5c3974f5c97710564c8ded7
John Kosta <kostajohn@ymail.com>
2020-10-07 23:25:30 -0700

    d27

--- d27.java
+++ d27.java
@@ -27,3 +27,80 @@ public String frontTimes(String str, int n) {
   }
   return frontRepeat;
 }
+
+//logic1 cigarParty
+public boolean cigarParty(int cigars, boolean isWeekend) {
+
+  if (isWeekend && cigars >= 40) {
+    return true;
+  } else if (!isWeekend && (cigars>=40 && cigars <= 60)) {
+    return true;
+  } else {
+    return false;
+  }
+
+
+
+}
+
+
+//String2 countHi
+public int countHi(String str) {
+
+  int count = 0;
+
+  for (int i = 0; i<str.length()-1;i++){
+    if (str.charAt(i)=='h' && str.charAt(i+1)=='i') {
+      count++;
+    }
+  } return count;
+
+
+}
+
+//String2 catDog
+public boolean catDog(String str) {
+  int catCount = 0;
+  int dogCount = 0;
+
+
+  for (int i = 0; i<str.length()-2; i++) {
+    if (str.charAt(i) == 'c' && str.charAt(i+1) == 'a' && str.charAt(i+2) == 't') {
+      catCount++;
+    }
+
+    if(str.charAt(i) == 'd' && str.charAt(i+1) == 'o' && str.charAt(i+2) == 'g') {
+      dogCount++;
+    }
+
+
+  } return catCount == dogCount;
+}
+
+//String2 xyzThere
+public boolean xyzThere(String str) {
+
+  boolean xyzAfterPeriod = false;
+
+
+   if(str.equals("xyz")) {
+      xyzAfterPeriod = true;
+    }
+
+
+
+  for (int i = 0; i<str.length()-3; i++) {
+    if (str.charAt(i) != '.' && str.charAt(i+1) == 'x' && str.charAt(i+2) == 'y' && str.charAt(i+3) == 'z') {
+      xyzAfterPeriod = true;
+    }
+
+
+
+   if (str.substring(0,3).equals("xyz")){
+    xyzAfterPeriod = true;
+   }
+
+  } return xyzAfterPeriod;
+
+
+}
