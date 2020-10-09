691eeaea1d228faacc31f7947c467c2b02ae8116
John Kosta <kostajohn@ymail.com>
2020-10-08 18:06:04 -0700

    d28

---
+++ d28.java
@@ -0,0 +1,13 @@
+//warmup1 monkeyTrouble
+public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+  return (aSmile && bSmile) || (!aSmile && !bSmile);
+}
+
+//warmup1 sumDouble
+public int sumDouble(int a, int b) {
+  if (a != b) {
+    return a + b;
+  } else {
+    return (a + b) * 2;
+  }
+}
