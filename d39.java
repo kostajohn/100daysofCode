020dee3b52b5ac9dd61ac7e510b118323ba4af81
John Kosta <kostajohn@ymail.com>
2020-10-20 09:09:18 -0700

    d39

--- d39.java
+++ d39.java
@@ -1,3 +1,4 @@
+//warmup1 delDel
 public String delDel(String str) {
   if (str.length() < 4) {
     return str;
@@ -6,4 +7,15 @@ public String delDel(String str) {
   } else {
     return str;
 }
+}
+
+//warmup1 mixStrart
+public boolean mixStart(String str) {
+  if (str.length() < 3) {
+    return false;
+  } else if (str.substring(1,3).equals("ix")){
+    return true;
+  } else {
+    return false;
+  }
 }
