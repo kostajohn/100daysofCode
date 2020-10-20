253ec32fb97e7d5b234d73c04ccb8d5306a629e9
John Kosta <kostajohn@ymail.com>
2020-10-19 18:15:40 -0700

    d38

---
+++ d38.java
@@ -0,0 +1,14 @@
+//warmup1 front22
+public String front22(String str) {
+  String result = "";
+
+
+  if (str.length() < 2) {
+    result = str + str + str;
+  } else {
+
+  result = str.substring(0,2) + str + str.substring(0,2);
+  }
+
+  return result;
+}
