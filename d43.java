891b5c4b8337873e4676d9f98817385fd325ee5f
John Kosta <kostajohn@ymail.com>
2020-10-28 09:01:58 -0700

    43

---
+++ d43.java
@@ -0,0 +1,14 @@
+
+//logic1 sortaSum
+public int sortaSum(int a, int b) {
+
+  int sum = 0;
+
+  if (a + b >=10 && (a + b) <= 19) {
+    sum = 20;
+  } else {
+    sum = a + b;
+  }
+
+  return sum;
+}
